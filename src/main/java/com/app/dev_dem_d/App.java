9c8e97004a1d30ad4aa858801d3490ec6df169d1
Lavanya <lavanya.h.27.it@psvpec.in>
2026-05-27 00:56:55 +0530

    Initial commit

---
+++ src/main/java/com/app/dev_dem_d/App.java
@@ -0,0 +1,9 @@
+package com.app.dev_dem_d;
+/**
+ * Hello world!
+ */
+public class App {
+    public int add (int a , int b) {
+        return (a+b);
+    }
+}
---
+++ src/test/java/com/app/dev_dem_d/AppTest.java
@@ -0,0 +1,14 @@
+package com.app.dev_dem_d;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class AppTest {
+
+    @Test
+    void testAddition() {
+        int sum = 2 + 3;
+        assertEquals(5, sum);
+    }
+}
